5d3251588c1fad8b09ef3513afd8d5d79ed797d4
Anton Romanovich <anthony.romanovich@gmail.com>
2012-11-23 17:28:19 +0600

    Add create_db command to manage.py

--- manage.py
+++ manage.py
@@ -3,10 +3,16 @@
 import nose
 from flask.ext.script import Manager
 
-from app import app
+from app import app, db
 
 
 manager = Manager(app)
 
+@manager.command
+def create_db():
+    """Creates all tables."""
+    db.create_all()
+
+    
 if __name__ == '__main__':
     manager.run()
